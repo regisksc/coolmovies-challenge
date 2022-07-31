6cbbbde7b7f61ba475ab2ddeb21f7e241d5f515e
Regis Kian <regisksc@gmail.com>
2022-07-31 20:28:27 -0300

    ref: remove remaining main.dart comments

--- coolmovies_mobile/lib/main.dart
+++ coolmovies_mobile/lib/main.dart
@@ -6,8 +6,6 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'core/core.dart';
 
 void main() async {
-  // We're using HiveStore for persistence,
-  // so we need to initialize Hive.
   await initHiveForFlutter();
 
   final HttpLink httpLink = HttpLink(
@@ -38,7 +36,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -89,16 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
@@ -155,7 +144,7 @@ Good luck! :)""",
             ),
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
