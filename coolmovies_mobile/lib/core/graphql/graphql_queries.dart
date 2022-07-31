2ae7dce4d4594ae3130fcab7ad86a0ceb1486f73
Regis Kian <regisksc@gmail.com>
2022-07-31 14:55:16 -0300

    ref: extracts allMoviesQuery into a static getter

---
+++ coolmovies_mobile/lib/core/graphql/graphql_queries.dart
@@ -0,0 +1,26 @@
+class GQLQueries {
+  GQLQueries._();
+
+  static String get allMovies => """
+          query AllMovies {
+            allMovies {
+              nodes {
+                id
+                imgUrl
+                movieDirectorId
+                userCreatorId
+                title
+                releaseDate
+                nodeId
+                userByUserCreatorId {
+                  id
+                  name
+                  nodeId
+                }
+              }
+            }
+          }
+        """;
+
+  // ##
+}
