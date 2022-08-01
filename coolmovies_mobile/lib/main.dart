f89e801fd9522e1fd3d19e50ad4fc6e353827af5
Regis Kian <regisksc@gmail.com>
2022-08-01 14:17:40 -0300

    feature: separates current UI into a page

--- coolmovies_mobile/lib/main.dart
+++ coolmovies_mobile/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'core/core.dart';
-import 'repositories/movie/concrete_movie_repository.dart';
+import 'pages/list_movies/list_movies_page.dart';
 
 void main() async {
   await initHiveForFlutter();
@@ -30,103 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Coolmovies'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final ValueNotifier<Map<String, dynamic>?> _data = ValueNotifier(null);
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  Future _fetchData() async {
-    debugPrint('Fetching data...');
-    final client = GraphQLProvider.of(context).value;
-    final repository = ConcreteMovieRepository(client);
-    final result = await repository.getAllMovies();
-    result.fold(
-      (left) => null,
-      (movies) => _data.value = {
-        "movies": movies.map((movie) => movie.toJson).toList().toString()
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.only(top: 36.0),
-                  child: Text(
-                    """
-Thank you for taking the time to take our test. We really appreciate it.
-All the information on what is required can be found in the README at the root of this repo.
-Please dont spend ages on this and just get through as much of it as you can.
-Good luck! :)""",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                TextButton.icon(
-                  onPressed: _incrementCounter,
-                  icon: const Icon(Icons.add),
-                  label: Text('Increment: $_counter'),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.blue,
-                  ),
-                ),
-                OutlinedButton.icon(
-                  onPressed: _fetchData,
-                  icon: const Icon(Icons.download),
-                  label: const Text('Fetch data'),
-                ),
-                const SizedBox(height: 16),
-                ValueListenableBuilder(
-                  valueListenable: _data,
-                  builder: (BuildContext context, Map<String, dynamic>? data,
-                      Widget? _) {
-                    return data != null
-                        ? Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                                color: Colors.grey[300],
-                                border: Border.all(color: Colors.grey.shade700),
-                                borderRadius: BorderRadius.circular(4)),
-                            child: Text(data.toString()),
-                          )
-                        : Container();
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: const ListMoviesPage(title: 'Coolmovies'),
     );
   }
 }
---
+++ coolmovies_mobile/lib/pages/list_movies/list_movies_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../../repositories/repositories.dart';
+
+class ListMoviesPage extends StatefulWidget {
+  const ListMoviesPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<ListMoviesPage> createState() => _ListMoviesPageState();
+}
+
+class _ListMoviesPageState extends State<ListMoviesPage> {
+  final ValueNotifier<Map<String, dynamic>?> _data = ValueNotifier(null);
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Future _fetchData() async {
+    debugPrint('Fetching data...');
+    final client = GraphQLProvider.of(context).value;
+    final repository = ConcreteMovieRepository(client);
+    final result = await repository.getAllMovies();
+    result.fold(
+      (left) => null,
+      (movies) => _data.value = {
+        "movies": movies.map((movie) => movie.toJson).toList().toString()
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 36.0),
+                  child: Text(
+                    """
+Thank you for taking the time to take our test. We really appreciate it.
+All the information on what is required can be found in the README at the root of this repo.
+Please dont spend ages on this and just get through as much of it as you can.
+Good luck! :)""",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextButton.icon(
+                  onPressed: _incrementCounter,
+                  icon: const Icon(Icons.add),
+                  label: Text('Increment: $_counter'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+                OutlinedButton.icon(
+                  onPressed: _fetchData,
+                  icon: const Icon(Icons.download),
+                  label: const Text('Fetch data'),
+                ),
+                const SizedBox(height: 16),
+                ValueListenableBuilder(
+                  valueListenable: _data,
+                  builder: (BuildContext context, Map<String, dynamic>? data,
+                      Widget? _) {
+                    return data != null
+                        ? Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                border: Border.all(color: Colors.grey.shade700),
+                                borderRadius: BorderRadius.circular(4)),
+                            child: Text(data.toString()),
+                          )
+                        : Container();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
