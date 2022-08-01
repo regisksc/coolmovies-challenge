76266044b120f9ab72bd9fc0627e4b2efbd58214
Regis Kian <regisksc@gmail.com>
2022-08-01 03:01:10 -0300

    build: add graphql utils

--- coolmovies_mobile/lib/main.dart
+++ coolmovies_mobile/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
@@ -8,22 +6,10 @@ import 'core/core.dart';
 void main() async {
   await initHiveForFlutter();
 
-  final HttpLink httpLink = HttpLink(
-    Platform.isAndroid
-        ? 'http://10.0.2.2:5001/graphql'
-        : 'http://localhost:5001/graphql',
-  );
-
-  final AuthLink authLink = AuthLink(
-    getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
-  );
-
-  final Link link = authLink.concat(httpLink);
-
   final client = ValueNotifier(
     GraphQLClient(
       link: link,
-      cache: GraphQLCache(store: HiveStore()),
+      cache: gqlCache,
     ),
   );
 
