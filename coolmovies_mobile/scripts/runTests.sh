16bc96709d5c666c2688f102370f954254319dc8
Regis Kian <regisksc@gmail.com>
2022-08-02 03:03:36 -0300

    ci: assign minimum coverage to 60%

--- coolmovies_mobile/scripts/runTests.sh
+++ coolmovies_mobile/scripts/runTests.sh
@@ -8,6 +8,6 @@ echo '############################### Running tests ############################
 cd ./coolmovies_mobile
 flutter test --coverage
 echo '############################### Removing generated files #####################'
-flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
+flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
 echo '############################### Generating coverage ##########################'
 genhtml coverage/lcov.info -o coverage/html
