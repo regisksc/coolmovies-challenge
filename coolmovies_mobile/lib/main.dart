93055ba01f9fe62d0ebc08ac03915f501bd1c6c8
Regis Kian <regisksc@gmail.com>
2022-08-02 21:29:41 -0300

    ref: adds app_widget and refactors MaterialApp

--- coolmovies_mobile/lib/main.dart
+++ coolmovies_mobile/lib/main.dart
@@ -1,45 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
+import 'app/app_widget.dart';
 import 'core/core.dart';
-import 'pages/list_movies/list_movies_page.dart';
 
 void main() async {
   await initHiveForFlutter();
 
-  final client = ValueNotifier(
-    GraphQLClient(
-      link: link,
-      cache: gqlCache,
-    ),
-  );
+  final graphql = GraphQLClient(link: link, cache: gqlCache);
+  final client = ValueNotifier(graphql);
 
   runApp(GraphQLProvider(
     client: client,
     child: const MyApp(),
   ));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const title = 'Coolmovies';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: MyApp.title,
-      debugShowCheckedModeBanner: false,
-      builder: (context, widget) => ResponsiveWrapper.builder(
-        ClampingScrollWrapper.builder(context, widget!),
-        breakpoints: const [
-          ResponsiveBreakpoint.resize(350, name: MOBILE),
-          ResponsiveBreakpoint.autoScale(600, name: TABLET),
-        ],
-      ),
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ListMoviesPage(title: MyApp.title),
-    );
-  }
 }
