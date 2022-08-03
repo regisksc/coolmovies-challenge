3d20969e72294874112b4a059ce5b65585ac6e1d
Regis Kian <regisksc@gmail.com>
2022-08-02 21:28:17 -0300

    feature: adds responsive_framework

--- coolmovies_mobile/lib/main.dart
+++ coolmovies_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'core/core.dart';
 import 'pages/list_movies/list_movies_page.dart';
@@ -23,14 +24,22 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const title = 'Coolmovies';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: MyApp.title,
+      debugShowCheckedModeBanner: false,
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+        ],
       ),
-      home: const ListMoviesPage(title: 'Coolmovies'),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ListMoviesPage(title: MyApp.title),
     );
   }
 }
