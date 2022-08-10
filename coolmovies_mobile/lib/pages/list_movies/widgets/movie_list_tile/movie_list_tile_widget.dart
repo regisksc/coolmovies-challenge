7664d1325d1dde157b875750627c3151b4a857d0
Regis Kian <regisksc@gmail.com>
2022-08-10 20:10:55 -0300

    ref: adds stack to movie list

--- coolmovies_mobile/lib/pages/list_movies/widgets/movie_list_tile/movie_list_tile_widget.dart
+++ coolmovies_mobile/lib/pages/list_movies/widgets/movie_list_tile/movie_list_tile_widget.dart
@@ -17,12 +17,13 @@ class MovieListTile extends StatelessWidget {
     final navigator = Navigator.of(context);
     final size = MediaQuery.of(context).size;
     final width = size.width;
-    final movieImageSize = width * .9;
+    final movieImageSize = width;
     return GestureDetector(
       onTap: () => navigator.pushNamed(AppRoutes.movie, arguments: movie),
       child: Container(
         height: movieImageSize * (context.screenIsSmall ? 1.1 : 1.35),
         width: movieImageSize,
+        margin: EdgeInsets.only(top: context.height * .19),
         decoration: BoxDecoration(
           color: const Color(0xfffdfdfd),
           borderRadius: defaultRadius,
--- coolmovies_mobile/lib/pages/list_movies/widgets/movie_list_widget.dart
+++ coolmovies_mobile/lib/pages/list_movies/widgets/movie_list_widget.dart
@@ -38,71 +38,72 @@ class _MoviesListState extends State<MoviesList> {
 
   @override
   Widget build(BuildContext context) {
-    final spacing = context.width * .1;
-    final containerHeight = context.height * .65;
-    return Consumer<MoviesProvider>(
-      builder: (_, provider, __) {
-        return Column(
-          children: [
-            SizedBox(
-              width: context.width,
-              height: containerHeight,
-              child: Builder(builder: (context) {
-                if (widget.movies.isEmpty) {
-                  const loader = CircularProgressIndicator.adaptive();
-                  return const Center(child: loader);
-                }
-                return PageView.builder(
-                  controller: _pageController,
-                  itemCount: widget.movies.length,
-                  // pageSnapping: false,
-                  itemBuilder: (BuildContext context, int index) {
-                    final movie = widget.movies[index];
-                    final vertPad = context.height * .02;
-                    final pageMovementMultiplier = index - _currentPage + 1;
-                    final translate =
-                        containerHeight * (1.05 - pageMovementMultiplier).abs();
-                    return Transform(
-                      transform: Matrix4.identity()
-                        ..scale(-pageMovementMultiplier * .5)
-                        ..translate(-1.1, translate)
-                        ..setRotationX(pi * 2 * pageMovementMultiplier)
-                        ..setRotationY(pi / 10 * pageMovementMultiplier)
-                        ..setRotationZ((-pi / 160) * pageMovementMultiplier)
-                        ..setEntry(3, 1, 0.0001),
-                      child: Column(
-                        children: [
-                          Container(
-                            key: const Key('movieTile'),
-                            padding: EdgeInsets.symmetric(vertical: vertPad),
-                            child: MovieListTile(movie),
-                          ),
-                          SizedBox(width: spacing),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              }),
-            ),
-            Visibility(
-              visible: provider.lastRequestFailure != null,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: context.width * .2),
-                child: AutoSizeText(
-                  """a problem occured when fetching movies. you might be seeing results from storage.""",
-                  minFontSize: 8,
-                  maxFontSize: 14,
-                  style: context.textTheme.labelSmall!.copyWith(
-                    fontSize: context.width * .03,
-                    color: Colors.redAccent.withOpacity(.4),
+    final containerHeight = context.height * .86;
+    return Container(
+      height: context.height,
+      padding: EdgeInsets.symmetric(vertical: context.height * .03),
+      child: Consumer<MoviesProvider>(
+        builder: (_, provider, __) {
+          return Column(
+            children: [
+              SizedBox(
+                width: context.width,
+                height: containerHeight,
+                child: Builder(builder: (context) {
+                  if (widget.movies.isEmpty) {
+                    const loader = CircularProgressIndicator.adaptive();
+                    return const Center(child: loader);
+                  }
+                  return PageView.builder(
+                    controller: _pageController,
+                    itemCount: widget.movies.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final movie = widget.movies[index];
+                      final vertPad = context.height * .02;
+                      final pageMovementMultiplier = index - _currentPage + 1;
+                      final translate = containerHeight *
+                          (1.05 - pageMovementMultiplier).abs();
+                      return Transform(
+                        transform: Matrix4.identity()
+                          ..scale(-pageMovementMultiplier * .15)
+                          ..translate(-1.1, translate)
+                          ..setRotationX(-pi * 4 * pageMovementMultiplier)
+                          ..setRotationY(pi / 4 * pageMovementMultiplier)
+                          ..setRotationZ((-pi / 200) * pageMovementMultiplier)
+                          ..setEntry(3, 1, 0.0001),
+                        child: Column(
+                          children: [
+                            Container(
+                              key: const Key('movieTile'),
+                              padding: EdgeInsets.symmetric(vertical: vertPad),
+                              child: MovieListTile(movie),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }),
+              ),
+              Visibility(
+                visible: provider.lastRequestFailure != null,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: context.width * .2),
+                  child: AutoSizeText(
+                    """a problem occured when fetching movies. you might be seeing results from storage.""",
+                    minFontSize: 8,
+                    maxFontSize: 14,
+                    style: context.textTheme.labelSmall!.copyWith(
+                      fontSize: context.width * .03,
+                      color: Colors.redAccent.withOpacity(.4),
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
-        );
-      },
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
