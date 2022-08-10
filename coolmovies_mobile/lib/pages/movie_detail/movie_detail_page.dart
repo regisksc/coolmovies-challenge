902932bf3df3fd89a3b40d577190e95ddaae53be
Regis Kian <regisksc@gmail.com>
2022-08-10 03:27:25 -0300

    feature: adds movie detail page app bar animation

--- coolmovies_mobile/lib/pages/movie_detail/movie_detail_page.dart
+++ coolmovies_mobile/lib/pages/movie_detail/movie_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,11 +17,13 @@ class MovieDetailPage extends StatefulWidget {
 class _MovieDetailPageState extends State<MovieDetailPage> {
   late MovieModel movie;
   late ScrollController scrollController;
+  late bool userPassedReviews;
 
   @override
   void initState() {
     super.initState();
     scrollController = ScrollController();
+    userPassedReviews = false;
   }
 
   @override
@@ -29,10 +32,18 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
     movie = ModalRoute.of(context)!.settings.arguments! as MovieModel;
     widget.provider.getReviewsForMovieId(movie.id);
     scrollController.addListener(() {
-      final pos = scrollController.position;
-      if (pos.atEdge && pos.pixels != 0) {
-        widget.provider.getReviewsForMovieId(movie.id);
-      }
+      setState(() {
+        final pos = scrollController.position;
+        if (pos.atEdge && pos.pixels != 0) {
+          widget.provider.getReviewsForMovieId(movie.id);
+        }
+        if (scrollController.position.pixels >=
+            (context.height * (context.screenIsSmall ? 1 : .5))) {
+          userPassedReviews = true;
+        } else {
+          userPassedReviews = false;
+        }
+      });
     });
   }
 
@@ -56,6 +67,8 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
               expandedHeight: context.height * .50,
               leading: BackToListButton(movie: movie),
               leadingWidth: context.width * .28,
+              pinned: true,
+              title: _PageTitle(movie, userPassedReviews: userPassedReviews),
               flexibleSpace: FlexibleSpaceBar(
                   background: MovieDetailHeader(movie),
                   stretchModes: const [
@@ -74,6 +87,37 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
               ]),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PageTitle extends StatelessWidget {
+  const _PageTitle(
+    this.movie, {
+    required this.userPassedReviews,
+    Key? key,
+  }) : super(key: key);
+
+  final bool userPassedReviews;
+  final MovieModel movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        right: context.width * .2,
+        left: context.width * .08,
+      ),
+      child: AnimatedOpacity(
+        duration: const Duration(milliseconds: 300),
+        opacity: userPassedReviews ? 1 : 0,
+        child: AutoSizeText(
+          movie.title,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.blueGrey[50]),
         ),
       ),
     );
--- coolmovies_mobile/lib/pages/movie_detail/widgets/movie_detailI_info/movie_detail_info_widget.dart
+++ coolmovies_mobile/lib/pages/movie_detail/widgets/movie_detailI_info/movie_detail_info_widget.dart
@@ -14,7 +14,7 @@ class MovieDetailInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: context.height * (context.screenIsSmall ? .35 : .3),
+      height: context.height * (context.screenIsSmall ? .4 : .3),
       padding: EdgeInsets.symmetric(horizontal: context.width * .02),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
