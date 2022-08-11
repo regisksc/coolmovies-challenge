373df9aea36af740066b5bfcd2e8b98680d74f1a
Regis Kian <regisksc@gmail.com>
2022-08-11 05:23:06 -0300

    test: test review fetching in movie provider

--- coolmovies_mobile/test/providers/movie_provider_test.dart
+++ coolmovies_mobile/test/providers/movie_provider_test.dart
@@ -1,6 +1,8 @@
+import 'package:coolmovies/core/models/models.dart';
 import 'package:coolmovies/providers/movie_provider.dart';
 import 'package:coolmovies/repositories/repositories.dart';
 import 'package:either_dart/either.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -61,61 +63,87 @@ void main() {
     },
   );
 
-  test(
-    "should replace review on edit save action",
-    () async {
-      // Arrange
-      when(() => repository.storeMovies(any()))
-          .thenAnswer((_) => Future.value());
-      when(() => repository.remoteAddReview(
-            movieId: any(named: 'movieId'),
-            userId: any(named: 'userId'),
-            review: any(named: 'review'),
-          )).thenAnswer((_) => Future.value());
-      when(() => repository.remoteEditReview(
-            movieId: any(named: 'movieId'),
-            userId: any(named: 'userId'),
-            review: any(named: 'review'),
-          )).thenAnswer((_) => Future.value());
-      when(() => repository.storeMovies(any()))
-          .thenAnswer((_) => Future.value());
-      final movies = mockMovieList();
-      final user = mockUserModel;
-      final reviewToBeEdited = movies[0].reviews[0];
-      final firstTitle = reviewToBeEdited.title;
-      // Act
-      sut.startEditingReview(reviewToBeEdited);
-      reviewToBeEdited.title = 'newTitle';
-      reviewToBeEdited.title = 'body';
-      sut.stopEditingReview(user, reviewToBeEdited, shouldSave: true);
-      final newTitle = reviewToBeEdited.title;
-      // Assert
-      verify(() => repository.storeMovies(any()));
-      verify(() => repository.remoteEditReview(
-            movieId: any(named: 'movieId'),
-            userId: any(named: 'userId'),
-            review: any(named: 'review'),
-          ));
-      expect(newTitle, isNot(firstTitle));
-    },
-  );
+  group('review context tests', () {
+    test(
+      "should get 3 reviews for a movie per request on success",
+      () async {
+        // Arrange
+        final fromRepository =
+            List<MovieReviewModel>.generate(3, (i) => mockMovieReviewModel);
+        when(() =>
+                repository.getMovieReviewsFor(any(), page: any(named: 'page')))
+            .thenAnswer((_) async => Right(fromRepository));
+        // Act
+        final id = faker.guid.guid();
+        await sut.getReviewsForMovieId(id);
+        final models = sut.reviews;
+        // Assert
+        await Future.delayed(
+          const Duration(milliseconds: 2011),
+          () {
+            verify(() =>
+                repository.getMovieReviewsFor(any(), page: any(named: 'page')));
+            expect(models[id]!.length, 3);
+          },
+        );
+      },
+    );
 
-  test(
-    "should not replace review on edit cancel action",
-    () async {
-      // Arrange
-      final movies = mockMovieList();
-      final user = mockUserModel;
-      final reviewToBeEdited = movies[0].reviews[0];
-      final titleBefore = reviewToBeEdited.title;
-      // Act
-      sut.startEditingReview(reviewToBeEdited);
-      reviewToBeEdited.title = 'newTitle';
-      sut.stopEditingReview(user, reviewToBeEdited);
-      final titleAfter = reviewToBeEdited.title;
-      // Assert
-      verifyNever(() => repository.storeMovies(any()));
-      expect(titleBefore, equals(titleAfter));
-    },
-  );
+    test(
+      "should replace review on edit save action",
+      () async {
+        // Arrange
+        when(() => repository.storeMovies(any()))
+            .thenAnswer((_) => Future.value());
+        when(() => repository.remoteAddReview(
+              movieId: any(named: 'movieId'),
+              userId: any(named: 'userId'),
+              review: any(named: 'review'),
+            )).thenAnswer((_) => Future.value());
+        when(() => repository.remoteEditReview(
+              movieId: any(named: 'movieId'),
+              userId: any(named: 'userId'),
+              review: any(named: 'review'),
+            )).thenAnswer((_) => Future.value());
+        when(() => repository.storeMovies(any()))
+            .thenAnswer((_) => Future.value());
+        final review = mockMovieReviewModel;
+        final user = mockUserModel;
+        final reviewToBeEdited = review;
+        final firstTitle = reviewToBeEdited.title;
+        // Act
+        sut.startEditingReview(reviewToBeEdited);
+        reviewToBeEdited.title = 'newTitle';
+        reviewToBeEdited.title = 'body';
+        sut.stopEditingReview(user, reviewToBeEdited, shouldSave: true);
+        final newTitle = reviewToBeEdited.title;
+        // Assert
+        verify(() => repository.storeMovies(any()));
+        verify(() => repository.remoteEditReview(
+              movieId: any(named: 'movieId'),
+              userId: any(named: 'userId'),
+              review: any(named: 'review'),
+            ));
+        expect(newTitle, isNot(firstTitle));
+      },
+    );
+
+    test(
+      "should not replace review on edit cancel action",
+      () async {
+        // Arrange
+        final review = mockMovieReviewModel;
+        final user = mockUserModel;
+        final titleBefore = review.title;
+        // Act
+        sut.startEditingReview(review);
+        review.title = 'newTitle';
+        sut.stopEditingReview(user, review);
+        final titleAfter = review.title;
+        // Assert
+        verifyNever(() => repository.storeMovies(any()));
+        expect(titleBefore, equals(titleAfter));
+      },
+    );
+  });
 }
